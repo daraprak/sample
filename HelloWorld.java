3caf843efd2b77fd023fb79a8a8092ea9e8fbbf2
daraprak <84379193+daraprak@users.noreply.github.com>
2024-06-05 11:06:08 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
         sayHello();
+        sayBye();
     }
 }
 
 public void sayHello() {
-    System.out.println("Hello, World!");
+    System.out.println("Hello World!");
+}
+
+public void sayBye() {
+    System.out.println("Bye Bye World!");
 }
